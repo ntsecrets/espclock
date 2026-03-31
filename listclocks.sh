9a0952f0cb7e6a8feb4246851b7de2e35751003f
ntsecrets <memcunning@gmail.com>
2026-03-31 12:22:01 -0400

    created listclocks.sh for macOS - lists active clocks on network

---
+++ listclocks.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+SERVICE="_http._tcp"
+
+dns-sd -Z "$SERVICE" local 2>/dev/null | awk '
+/PTR/ && /(espclock-|eyedro-)/ {
+    name=$NF
+    sub(/\._http._tcp$/, "", name)
+
+    type="unknown"
+    version="unknown"
+    fname="unknown"
+
+    # read until we hit TXT line
+    while (getline) {
+        if ($0 ~ /TXT/) {
+            # extract quoted fields
+            while (match($0, /"[^"]+"/)) {
+                field = substr($0, RSTART+1, RLENGTH-2)
+                sub(/^[^=]+=/, "", field_value)
+                
+                split(field, kv, "=")
+
+                if (kv[1] == "type") type = kv[2]
+                if (kv[1] == "version") version = kv[2]
+                if (kv[1] == "name") fname = kv[2]
+
+                $0 = substr($0, RSTART + RLENGTH)
+            }
+
+            print "---- " name " ----"
+            print "Type:    " type
+            print "Version: " version
+            print "Name:    " fname
+            print ""
+            break
+        }
+    }
+}
+'
